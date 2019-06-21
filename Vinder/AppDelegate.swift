7b720ec1a155ccff9a9587ed9d4fb50e6ed553bc
Frank Chen <frankcrestview@hotmail.com>
2019-06-21 17:51:50 -0400

    basic notification

--- Vinder/AppDelegate.swift
+++ Vinder/AppDelegate.swift
@@ -9,15 +9,39 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import UserNotifications
+import FirebaseMessaging
+import FirebaseInstanceID
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
   
   var window: UIWindow?
   
+  func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+    print(remoteMessage.appData)
+  }
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+      // For iOS 10 data message (sent via FCM
+      Messaging.messaging().delegate = self
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+    
+    application.registerForRemoteNotifications()
     FirebaseApp.configure()
+    
     
     self.window = UIWindow(frame:UIScreen.main.bounds)
 
